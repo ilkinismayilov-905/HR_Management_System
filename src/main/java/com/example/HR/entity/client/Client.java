f336cfbf93ab140c926a4aa5dcc5d99464881546
ilkinismayilov-905 <ismayilovilkin2006@gmail.com>
2025-09-17 02:26:24 +0400

    client class

---
+++ src/main/java/com/example/HR/entity/client/Client.java
@@ -0,0 +1,51 @@
+package com.example.HR.entity.client;
+
+import com.example.HR.validation.Create;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "clients")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@RequiredArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    @Column(unique = true)
+    private String clientId;
+
+    @Column(nullable = false)
+    private String clientName;
+
+    @Column(nullable = false,unique = true)
+    private String companyName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(groups = Create.class,message = "Email is required")
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid phone number")
+    private String phoneNumber;
+
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
